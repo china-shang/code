a2cf5f8b1b922fd7e1cd25e4a263f6b108ac6def
china-shang <hutheshang@gmail.com>
2021-12-02 14:11:27 +0800

    fixup KMP

--- KMP.java
+++ KMP.java
@@ -1,31 +1,77 @@
+import java.util.Arrays;
+
 class KMP {
 
     public static int kmp(String s1, String s2){
         int m=s1.length(), n=s2.length();
-        if(n==0){
-            return -1;
+        if(n == 0){
+            return 0;
         }
-        int[] pi = new int[n];
-
-        for(int i=1,j=0;i<n;i++){
-            while(j > 0 && s2.charAt(i) != s2.charAt(j)){
+        int[] pi=new int[n];
+        for(int i=1,j=0; i<n;i++){
+            while(j>0 && s2.charAt(i) != s2.charAt(j)){
                 j = pi[j-1];
             }
-            if(s2.charAt(i) == s2.charAt(j)){
+            if( s2.charAt(i) == s2.charAt(j)){
                 j++;
             }
             pi[i] = j;
         }
 
-        for(int i=0,j=0; i<m; i++){
-            while(j > 0 && s1.charAt(i) != s2.charAt(j)){
-                j=pi[j-1];
+        // for (int i = 0, j = 0; i < m; i++) {
+        for(int i=0,j=0; i < m; i++){
+            // while (j > 0 && s1.charAt(i) != s2.charAt(j)) {
+            while()
+                j = pi[j - 1];
             }
-            if(s1.charAt(i) == s2.charAt(j)){
+            if (s1.charAt(i) == s2.charAt(j)) {
                 j++;
             }
-           if(j == n){
+            if (j == n) {
                 return i - n + 1;
+            }
+        }
+
+        // for(int i=0,j=0;i<m;i++){
+            // if(j>0 && s1.charAt(i) != s2.charAt(j)){
+                // j = pi[j-1];
+            // }
+            // if( s1.charAt(i) == s2.charAt(j)){
+                // j++;
+            // }
+            // if(n == j){
+                // return i - n + 1;
+            // }
+        // }
+
+
+        return -1;
+    }
+
+    public static int strStr(String s1, String s2) {
+        int n = s1.length(), m = s2.length();
+        if (m == 0) {
+            return 0;
+        }
+        int[] pi = new int[m];
+        for (int i = 1, j = 0; i < m; i++) {
+            while (j > 0 && s2.charAt(i) != s2.charAt(j)) {
+                j = pi[j - 1];
+            }
+            if (s2.charAt(i) == s2.charAt(j)) {
+                j++;
+            }
+            pi[i] = j;
+        }
+        for (int i = 0, j = 0; i < n; i++) {
+            while (j > 0 && s1.charAt(i) != s2.charAt(j)) {
+                j = pi[j - 1];
+            }
+            if (s1.charAt(i) == s2.charAt(j)) {
+                j++;
+            }
+            if (j == m) {
+                return i - m + 1;
             }
         }
         return -1;
@@ -33,8 +79,12 @@ class KMP {
 
 
 
+
     public static void main(String[] args) {
-        System.out.println(kmp("asdfjk", "dfjk"));
+        System.out.println(kmp("ababaabbbbababbaabaaabaabbaaaabbabaabbbbbbabbaabbabbbabbbbbaaabaababbbaabbbabbbaabbbbaaabbababbabbbabaaabbaabbabababbbaaaaaaababbabaababaabbbbaaabbbabb",
+                    "abbabbbabaa"));
+        System.out.println(strStr("ababaabbbbababbaabaaabaabbaaaabbabaabbbbbbabbaabbabbbabbbbbaaabaababbbaabbbabbbaabbbbaaabbababbabbbabaaabbaabbabababbbaaaaaaababbabaababaabbbbaaabbbabb",
+                    "abbabbbabaa"));
 
 
     }
